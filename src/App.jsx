13b57db9c0e9f123a58cfb4e1d7db707f5f23ee6
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2023-01-30 13:30:42 -0300

    add elements in index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import ImgButton from "../src/assets/icon-dice.svg"
+import DivisionDesk from "../src/assets/pattern-divider-desktop.svg"
+
+import "../src/styles/components/app.sass"
+import "../src/styles/reset.sass"
 
 function App() {
   return (
-    <h1>hello world!</h1>
+    <section className="container">
+      <div className="main-box">
+        <img className="main-button" src={ImgButton} alt="Logo Button" />
+        <img className="main-division" src={DivisionDesk} alt="Division Desktop" />
+      </div>
+    </section>
   )
 }
 
