3a667ecf5ea2bfd8db7b134f6dd1643a9355b8d6
Marlon Monsores <64220885+monsor3s@users.noreply.github.com>
2023-02-11 14:48:18 -0300

    added id and advice text in page from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import ImgButton from "../src/assets/icon-dice.svg"
 import DivisionDesk from "../src/assets/pattern-divider-desktop.svg"
+import { useEffect, useState } from "react"
 
 import "../src/styles/components/app.sass"
 import "../src/styles/reset.sass"
 import "../src/scripts/index.js"
 
 function App() {
+  const [text, setText] = useState([])
+
+  const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    setText(data.slip)
+  } 
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [], {once: true})
+
   return (
     <section className="container">
       <div className="main-box">
         <div className="main-card">
-          <p id="advice-id"></p>
-          <div id="advice-text"></div>
+          <p id="advice-id">Advice #{text.id}</p>
+          <div id="advice-text">"{text.advice}"</div>
           <img className="main-division" src={DivisionDesk} alt="Division Desktop" />
         </div>
-        <button className="main-button">
+        <button className="main-button" onClick={fetchAdvice}>
           <img src={ImgButton} alt="Logo Button" />
         </button>
       </div>
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,29 @@
-const resDiv = document.querySelector("#advice-text");
-const resId = document.querySelector("#advice-id");
-const resBtn = document.querySelector(".main-button");
+// const resDiv = document.querySelector("#advice-text");
+// const resId = document.querySelector("#advice-id");
+// const resBtn = document.querySelector(".main-button");
 
 
-// window.onload = () => {
-//   getAdvice();
+// // window.onload = () => {
+// //   getAdvice();
+// // }
+
+// useEffect(() => {
+//   getAdvice()
+// }, [])
+
+// function getAdvice() {
+//   fetch('https://api.adviceslip.com/advice').then(response => 
+//   {
+//     return response.json();
+//   }).then(adviceData => {
+//     const Adviceobj = adviceData.slip;
+//     resId.innerHTML = `advice #${Adviceobj.id}`;
+//     resDiv.innerHTML = `${Adviceobj.advice}`;
+//   }).catch(error => {
+//     console.log(error);
+//   });
 // }
 
-useEffect(() => {
-  getAdvice()
-}, [])
-
-function getAdvice() {
-  fetch('https://api.adviceslip.com/advice').then(response => 
-  {
-    return response.json();
-  }).then(adviceData => {
-    const Adviceobj = adviceData.slip;
-    resId.innerHTML = `advice #${Adviceobj.id}`;
-    resDiv.innerHTML = `${Adviceobj.advice}`;
-  }).catch(error => {
-    console.log(error);
-  });
-}
-
-resBtn.addEventListener('click', () => {
-  getAdvice();
-})
+// resBtn.addEventListener('click', () => {
+//   getAdvice();
+// })
